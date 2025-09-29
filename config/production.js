c20b415b28c07286fb88a73103ba7c29343db512
Babu-Bhai-No-1 <rajmohitsaxena@gmail.com>
2025-09-30 05:10:31 +0530

    Fix database name in production config

--- config/production.js
+++ config/production.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "MONGODB_URI": "mongodb+srv://rajmohitsaxena_db_user:VeKY2P9JCr6KPL3m@cluster0.1ypljdg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "MONGODB_URI": "mongodb+srv://rajmohitsaxena_db_user:VeKY2P9JCr6KPL3m@cluster0.1ypljdg.mongodb.net/Luxira?retryWrites=true&w=majority&appName=Cluster0"
 };
