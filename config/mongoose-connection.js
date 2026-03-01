9003cc3356e072396c72ecabf07888ffd2bc417b
Babu-Bhai-No-1 <rajmohitsaxena@gmail.com>
2026-03-01 22:38:13 +0530

    Add logging to show which MongoDB URI is being used

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -3,9 +3,12 @@ const config = require('config');
 
 const dbgr = require('debug')("development:mongoose");
 
+const mongoUri = config.get("MONGODB_URI");
+console.log("📌 Attempting to connect to MongoDB...");
+console.log("🔗 Connection String:", mongoUri.substring(0, 50) + "...");
 
 mongoose
-.connect(config.get("MONGODB_URI"))
+.connect(mongoUri)
 .then(function(){
      dbgr("connected");
      console.log("✓ MongoDB connected successfully");
