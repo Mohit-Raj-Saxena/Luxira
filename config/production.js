77e4e34060ec9f669dc4bac0a921b4375c197394
Babu-Bhai-No-1 <rajmohitsaxena@gmail.com>
2026-03-01 23:45:05 +0530

    Update MongoDB credentials with correct password

--- config/production.js
+++ config/production.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "MONGODB_URI": "mongodb+srv://rajmohitsaxena_db_user:rajmohitsaxena_db_user@cluster1.r8ia5fe.mongodb.net/luxira?retryWrites=true&w=majority"
+    "MONGODB_URI": "mongodb+srv://rajmohitsaxena_db_user:BDrJQHK4osqsxLku@cluster1.r8ia5fe.mongodb.net/luxira?retryWrites=true&w=majority"
 };
